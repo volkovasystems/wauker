c09097ba0e0f5e91e9f28a3ac647518964e83f35
johnlenon <johnlenonmaghanoy@gmail.com>
2017-05-02 20:40:39 +0800

    Updated test.

--- wauker-test.js
+++ wauker-test.js
@@ -1,6 +1,8 @@
-
+const assert = require( "assert" );
 const wauker = require( "./wauker.js" );
 
-console.log( wauker( Array ) );
+assert.ok( wauker( Array ) );
 
-console.log( wauker( RangeError ).map( ( constructor ) => { return constructor.name; } ) );
+assert.ok( wauker( RangeError ).map( ( constructor ) => { return constructor.name; } ) );
+
+console.log( "ok" );
