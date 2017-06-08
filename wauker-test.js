ef16c828e263ac4d0a41d0345d607650a3e4e5fb
johnlenon <johnlenonmaghanoy@gmail.com>
2017-06-08 16:13:43 +0800

    Updated test.

--- wauker-test.js
+++ wauker-test.js
@@ -1,8 +1,23 @@
 const assert = require( "assert" );
 const wauker = require( "./wauker.js" );
 
-assert.ok( wauker( Array ) );
+assert.deepEqual( wauker( Array ).map( ( constructor ) => { return constructor.name; } ), [ "Array" ] );
 
-assert.ok( wauker( RangeError ).map( ( constructor ) => { return constructor.name; } ) );
+assert.deepEqual( wauker( Date ).map( ( constructor ) => { return constructor.name; } ), [ "Date" ] );
+
+assert.deepEqual( wauker( RegExp ).map( ( constructor ) => { return constructor.name; } ), [ "RegExp" ] );
+
+assert.deepEqual( wauker( RangeError ).map( ( constructor ) => { return constructor.name; } ), [ "RangeError", "Error" ] );
+
+function Guest( name, index ){
+	this.name = name;
+	return index;
+};
+
+Guest.prototype.addGuest = function addGuest( name ) {
+	return name;
+};
+
+assert.deepEqual( wauker( Guest ).map( ( constructor ) => { return constructor.name; } ), [ "Guest" ] );
 
 console.log( "ok" );
